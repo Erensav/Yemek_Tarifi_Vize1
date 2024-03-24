2d06c708e0dcc159fb0f6eedb522de17e9d41a68
EREN SAVASIR <erensavasir47@gmail.com>
2024-03-25 01:16:28 +0300

    video-6 sql baglanti

---
+++ Yemek_Tarifi_Vize1/sqlsinif.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+
+namespace Yemek_Tarifi_Vize1
+{
+    public class sqlsinif
+    {
+        public SqlConnection baglanti()
+        {
+            SqlConnection baglan = new SqlConnection(@"Data Source=EREN\SQLEXPRESS;Initial Catalog=Dbo_yemekTarifi;Integrated Security=True");
+            baglan.Open();
+            return baglan;
+        }
+
+    }
+}
